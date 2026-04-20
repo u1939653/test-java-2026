b5c194cf688ead6822dbf4f32efd6b1b32d80cd2
Jaume Oliveras Hernández <u1939653@gmail.com>
2026-04-20 22:31:19 +0200

    [PHASE 1] Added health endpoint to check whether server is up

--- TestJava2026/build.gradle.kts
+++ TestJava2026/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- TestJava2026/changelog.md
+++ TestJava2026/changelog.md
@@ -1,3 +1,4 @@
 # Version 0.0.1
 ## Phase 1 - Environment preparation
 - Build basic runnable SpringBoot server
+- Created health check endpoint
