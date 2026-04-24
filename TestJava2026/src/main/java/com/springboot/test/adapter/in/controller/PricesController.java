b989e793d78ff9cdf0c45aeafe4a8dc953b21ff1
Jaume Oliveras Hernández <u1939653@gmail.com>
2026-04-24 14:43:40 +0200

    [PHASE 6] Removing debug logs

--- TestJava2026/src/main/java/com/springboot/test/adapter/in/controller/PricesController.java
+++ TestJava2026/src/main/java/com/springboot/test/adapter/in/controller/PricesController.java
@@ -4,8 +4,6 @@ import com.springboot.test.adapter.in.dto.PriceResponse;
 import com.springboot.test.adapter.in.mapper.PriceResponseMapper;
 import com.springboot.test.application.port.in.GetBrandProductPriceUseCase;
 import com.springboot.test.domain.model.Prices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,6 @@ import java.time.LocalDateTime;
 
 @RestController
 public class PricesController {
-    private Logger logger = LoggerFactory.getLogger(PricesController.class);
     private final GetBrandProductPriceUseCase getBrandProductPriceUseCase;
     private final PriceResponseMapper priceResponseMapper;
 
@@ -29,9 +26,7 @@ public class PricesController {
 
     @GetMapping("/prices")
     public ResponseEntity<PriceResponse> getPriceAtDate(@RequestParam Integer brandId, @RequestParam Integer productId, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date ) {
-        this.logger.debug("[PricesController][getPriceAtDate][DEBUG] Start: brandId={}, productId={}, date={}", brandId, productId, date.toString());
         Prices currentPrice = this.getBrandProductPriceUseCase.getBrandProductPrice(brandId, productId, date);
-        this.logger.debug("[PricesController][getPriceAtDate][DEBUG] Price found: {}", currentPrice.getPriceId());
         return ResponseEntity.ok(this.priceResponseMapper.toResponse(currentPrice));
     }
 }
